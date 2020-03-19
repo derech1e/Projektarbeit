f87633a3d00662ceb1a8482e5e41a39d7de22727
Thomas <unconfigured@null.org>
2020-03-19 19:37:46 +0100

    GUI Update

---
+++ MarioKartRechner/FrmHaupt.Designer.cs
@@ -0,0 +1,412 @@
+﻿namespace MarioKartRechner
+{
+    partial class FrmHaupt
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBoxSchueler = new System.Windows.Forms.ListBox();
+            this.lblSchüler = new System.Windows.Forms.Label();
+            this.lblGame_1 = new System.Windows.Forms.Label();
+            this.listBoxGame_1 = new System.Windows.Forms.ListBox();
+            this.lblGame_2 = new System.Windows.Forms.Label();
+            this.listBoxGame_2 = new System.Windows.Forms.ListBox();
+            this.lblGame_3 = new System.Windows.Forms.Label();
+            this.listBoxGame_3 = new System.Windows.Forms.ListBox();
+            this.lblGame_4 = new System.Windows.Forms.Label();
+            this.listBoxGame_4 = new System.Windows.Forms.ListBox();
+            this.lblGame_5 = new System.Windows.Forms.Label();
+            this.listBoxGame_5 = new System.Windows.Forms.ListBox();
+            this.lblGame_6 = new System.Windows.Forms.Label();
+            this.listBoxGame_6 = new System.Windows.Forms.ListBox();
+            this.lblGame_7 = new System.Windows.Forms.Label();
+            this.listBoxGame_7 = new System.Windows.Forms.ListBox();
+            this.lblGame_8 = new System.Windows.Forms.Label();
+            this.listBoxGame_8 = new System.Windows.Forms.ListBox();
+            this.btnGame_1 = new System.Windows.Forms.Button();
+            this.btnGame_2 = new System.Windows.Forms.Button();
+            this.btnGame_3 = new System.Windows.Forms.Button();
+            this.btnGame_4 = new System.Windows.Forms.Button();
+            this.btnGame_5 = new System.Windows.Forms.Button();
+            this.btnGame_6 = new System.Windows.Forms.Button();
+            this.btnGame_7 = new System.Windows.Forms.Button();
+            this.btnGame_8 = new System.Windows.Forms.Button();
+            this.lblAddToGame = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listBoxSchueler
+            // 
+            this.listBoxSchueler.FormattingEnabled = true;
+            this.listBoxSchueler.ItemHeight = 18;
+            this.listBoxSchueler.Items.AddRange(new object[] {
+            "Fridolin",
+            "Marcus",
+            "Alois",
+            "Tommy",
+            "Pascal",
+            "Hans",
+            "Frieder",
+            "Alex",
+            "Jannik",
+            "Cora",
+            "Tobias",
+            "Anton",
+            "Louis",
+            "Richard",
+            "Leo",
+            "Thomas",
+            "Richard",
+            "Hannes",
+            "Julius",
+            "Louis",
+            "Luca-Leon",
+            "Kai-Henrik",
+            "Max",
+            "Mike",
+            "Vincent"});
+            this.listBoxSchueler.Location = new System.Drawing.Point(13, 32);
+            this.listBoxSchueler.Margin = new System.Windows.Forms.Padding(4);
+            this.listBoxSchueler.Name = "listBoxSchueler";
+            this.listBoxSchueler.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            this.listBoxSchueler.Size = new System.Drawing.Size(194, 652);
+            this.listBoxSchueler.TabIndex = 0;
+            this.listBoxSchueler.SelectedIndexChanged += new System.EventHandler(this.listBoxSchueler_SelectedIndexChanged);
+            // 
+            // lblSchüler
+            // 
+            this.lblSchüler.AutoSize = true;
+            this.lblSchüler.Location = new System.Drawing.Point(12, 9);
+            this.lblSchüler.Name = "lblSchüler";
+            this.lblSchüler.Size = new System.Drawing.Size(89, 18);
+            this.lblSchüler.TabIndex = 1;
+            this.lblSchüler.Text = "Alle Schüler:";
+            // 
+            // lblGame_1
+            // 
+            this.lblGame_1.AutoSize = true;
+            this.lblGame_1.Location = new System.Drawing.Point(460, 33);
+            this.lblGame_1.Name = "lblGame_1";
+            this.lblGame_1.Size = new System.Drawing.Size(56, 18);
+            this.lblGame_1.TabIndex = 4;
+            this.lblGame_1.Text = "Spiel 1:";
+            // 
+            // listBoxGame_1
+            // 
+            this.listBoxGame_1.FormattingEnabled = true;
+            this.listBoxGame_1.ItemHeight = 18;
+            this.listBoxGame_1.Location = new System.Drawing.Point(463, 54);
+            this.listBoxGame_1.Name = "listBoxGame_1";
+            this.listBoxGame_1.Size = new System.Drawing.Size(163, 184);
+            this.listBoxGame_1.TabIndex = 3;
+            // 
+            // lblGame_2
+            // 
+            this.lblGame_2.AutoSize = true;
+            this.lblGame_2.Location = new System.Drawing.Point(650, 33);
+            this.lblGame_2.Name = "lblGame_2";
+            this.lblGame_2.Size = new System.Drawing.Size(56, 18);
+            this.lblGame_2.TabIndex = 6;
+            this.lblGame_2.Text = "Spiel 2:";
+            // 
+            // listBoxGame_2
+            // 
+            this.listBoxGame_2.FormattingEnabled = true;
+            this.listBoxGame_2.ItemHeight = 18;
+            this.listBoxGame_2.Location = new System.Drawing.Point(653, 54);
+            this.listBoxGame_2.Name = "listBoxGame_2";
+            this.listBoxGame_2.Size = new System.Drawing.Size(163, 184);
+            this.listBoxGame_2.TabIndex = 5;
+            // 
+            // lblGame_3
+            // 
+            this.lblGame_3.AutoSize = true;
+            this.lblGame_3.Location = new System.Drawing.Point(838, 33);
+            this.lblGame_3.Name = "lblGame_3";
+            this.lblGame_3.Size = new System.Drawing.Size(56, 18);
+            this.lblGame_3.TabIndex = 8;
+            this.lblGame_3.Text = "Spiel 3:";
+            // 
+            // listBoxGame_3
+            // 
+            this.listBoxGame_3.FormattingEnabled = true;
+            this.listBoxGame_3.ItemHeight = 18;
+            this.listBoxGame_3.Location = new System.Drawing.Point(841, 54);
+            this.listBoxGame_3.Name = "listBoxGame_3";
+            this.listBoxGame_3.Size = new System.Drawing.Size(163, 184);
+            this.listBoxGame_3.TabIndex = 7;
+            // 
+            // lblGame_4
+            // 
+            this.lblGame_4.AutoSize = true;
+            this.lblGame_4.Location = new System.Drawing.Point(1027, 33);
+            this.lblGame_4.Name = "lblGame_4";
+            this.lblGame_4.Size = new System.Drawing.Size(56, 18);
+            this.lblGame_4.TabIndex = 10;
+            this.lblGame_4.Text = "Spiel 4:";
+            // 
+            // listBoxGame_4
+            // 
+            this.listBoxGame_4.FormattingEnabled = true;
+            this.listBoxGame_4.ItemHeight = 18;
+            this.listBoxGame_4.Location = new System.Drawing.Point(1030, 54);
+            this.listBoxGame_4.Name = "listBoxGame_4";
+            this.listBoxGame_4.Size = new System.Drawing.Size(163, 184);
+            this.listBoxGame_4.TabIndex = 9;
+            // 
+            // lblGame_5
+            // 
+            this.lblGame_5.AutoSize = true;
+            this.lblGame_5.Location = new System.Drawing.Point(1216, 33);
+            this.lblGame_5.Name = "lblGame_5";
+            this.lblGame_5.Size = new System.Drawing.Size(56, 18);
+            this.lblGame_5.TabIndex = 12;
+            this.lblGame_5.Text = "Spiel 5:";
+            // 
+            // listBoxGame_5
+            // 
+            this.listBoxGame_5.FormattingEnabled = true;
+            this.listBoxGame_5.ItemHeight = 18;
+            this.listBoxGame_5.Location = new System.Drawing.Point(1219, 54);
+            this.listBoxGame_5.Name = "listBoxGame_5";
+            this.listBoxGame_5.Size = new System.Drawing.Size(163, 184);
+            this.listBoxGame_5.TabIndex = 11;
+            // 
+            // lblGame_6
+            // 
+            this.lblGame_6.AutoSize = true;
+            this.lblGame_6.Location = new System.Drawing.Point(1407, 33);
+            this.lblGame_6.Name = "lblGame_6";
+            this.lblGame_6.Size = new System.Drawing.Size(56, 18);
+            this.lblGame_6.TabIndex = 14;
+            this.lblGame_6.Text = "Spiel 6:";
+            // 
+            // listBoxGame_6
+            // 
+            this.listBoxGame_6.FormattingEnabled = true;
+            this.listBoxGame_6.ItemHeight = 18;
+            this.listBoxGame_6.Location = new System.Drawing.Point(1410, 54);
+            this.listBoxGame_6.Name = "listBoxGame_6";
+            this.listBoxGame_6.Size = new System.Drawing.Size(163, 184);
+            this.listBoxGame_6.TabIndex = 13;
+            // 
+            // lblGame_7
+            // 
+            this.lblGame_7.AutoSize = true;
+            this.lblGame_7.Location = new System.Drawing.Point(1599, 33);
+            this.lblGame_7.Name = "lblGame_7";
+            this.lblGame_7.Size = new System.Drawing.Size(56, 18);
+            this.lblGame_7.TabIndex = 16;
+            this.lblGame_7.Text = "Spiel 7:";
+            // 
+            // listBoxGame_7
+            // 
+            this.listBoxGame_7.FormattingEnabled = true;
+            this.listBoxGame_7.ItemHeight = 18;
+            this.listBoxGame_7.Location = new System.Drawing.Point(1602, 54);
+            this.listBoxGame_7.Name = "listBoxGame_7";
+            this.listBoxGame_7.Size = new System.Drawing.Size(163, 184);
+            this.listBoxGame_7.TabIndex = 15;
+            // 
+            // lblGame_8
+            // 
+            this.lblGame_8.AutoSize = true;
+            this.lblGame_8.Location = new System.Drawing.Point(1793, 33);
+            this.lblGame_8.Name = "lblGame_8";
+            this.lblGame_8.Size = new System.Drawing.Size(56, 18);
+            this.lblGame_8.TabIndex = 18;
+            this.lblGame_8.Text = "Spiel 8:";
+            // 
+            // listBoxGame_8
+            // 
+            this.listBoxGame_8.FormattingEnabled = true;
+            this.listBoxGame_8.ItemHeight = 18;
+            this.listBoxGame_8.Location = new System.Drawing.Point(1796, 54);
+            this.listBoxGame_8.Name = "listBoxGame_8";
+            this.listBoxGame_8.Size = new System.Drawing.Size(163, 184);
+            this.listBoxGame_8.TabIndex = 17;
+            // 
+            // btnGame_1
+            // 
+            this.btnGame_1.Location = new System.Drawing.Point(214, 54);
+            this.btnGame_1.Name = "btnGame_1";
+            this.btnGame_1.Size = new System.Drawing.Size(110, 37);
+            this.btnGame_1.TabIndex = 19;
+            this.btnGame_1.Text = "Spiel 1";
+            this.btnGame_1.UseVisualStyleBackColor = true;
+            this.btnGame_1.Click += new System.EventHandler(this.btnGame_1_Click);
+            // 
+            // btnGame_2
+            // 
+            this.btnGame_2.Location = new System.Drawing.Point(214, 97);
+            this.btnGame_2.Name = "btnGame_2";
+            this.btnGame_2.Size = new System.Drawing.Size(110, 37);
+            this.btnGame_2.TabIndex = 20;
+            this.btnGame_2.Text = "Spiel 2";
+            this.btnGame_2.UseVisualStyleBackColor = true;
+            this.btnGame_2.Click += new System.EventHandler(this.btnGame_2_Click);
+            // 
+            // btnGame_3
+            // 
+            this.btnGame_3.Location = new System.Drawing.Point(214, 140);
+            this.btnGame_3.Name = "btnGame_3";
+            this.btnGame_3.Size = new System.Drawing.Size(110, 37);
+            this.btnGame_3.TabIndex = 21;
+            this.btnGame_3.Text = "Spiel 3";
+            this.btnGame_3.UseVisualStyleBackColor = true;
+            this.btnGame_3.Click += new System.EventHandler(this.btnGame_3_Click);
+            // 
+            // btnGame_4
+            // 
+            this.btnGame_4.Location = new System.Drawing.Point(214, 183);
+            this.btnGame_4.Name = "btnGame_4";
+            this.btnGame_4.Size = new System.Drawing.Size(110, 37);
+            this.btnGame_4.TabIndex = 22;
+            this.btnGame_4.Text = "Spiel 4";
+            this.btnGame_4.UseVisualStyleBackColor = true;
+            this.btnGame_4.Click += new System.EventHandler(this.btnGame_4_Click);
+            // 
+            // btnGame_5
+            // 
+            this.btnGame_5.Location = new System.Drawing.Point(214, 226);
+            this.btnGame_5.Name = "btnGame_5";
+            this.btnGame_5.Size = new System.Drawing.Size(110, 37);
+            this.btnGame_5.TabIndex = 23;
+            this.btnGame_5.Text = "Spiel 5";
+            this.btnGame_5.UseVisualStyleBackColor = true;
+            this.btnGame_5.Click += new System.EventHandler(this.btnGame_5_Click);
+            // 
+            // btnGame_6
+            // 
+            this.btnGame_6.Location = new System.Drawing.Point(214, 269);
+            this.btnGame_6.Name = "btnGame_6";
+            this.btnGame_6.Size = new System.Drawing.Size(110, 37);
+            this.btnGame_6.TabIndex = 24;
+            this.btnGame_6.Text = "Spiel 6";
+            this.btnGame_6.UseVisualStyleBackColor = true;
+            this.btnGame_6.Click += new System.EventHandler(this.btnGame_6_Click);
+            // 
+            // btnGame_7
+            // 
+            this.btnGame_7.Location = new System.Drawing.Point(214, 312);
+            this.btnGame_7.Name = "btnGame_7";
+            this.btnGame_7.Size = new System.Drawing.Size(110, 37);
+            this.btnGame_7.TabIndex = 25;
+            this.btnGame_7.Text = "Spiel 7";
+            this.btnGame_7.UseVisualStyleBackColor = true;
+            this.btnGame_7.Click += new System.EventHandler(this.btnGame_7_Click);
+            // 
+            // btnGame_8
+            // 
+            this.btnGame_8.Location = new System.Drawing.Point(214, 355);
+            this.btnGame_8.Name = "btnGame_8";
+            this.btnGame_8.Size = new System.Drawing.Size(110, 37);
+            this.btnGame_8.TabIndex = 26;
+            this.btnGame_8.Text = "Spiel 8";
+            this.btnGame_8.UseVisualStyleBackColor = true;
+            this.btnGame_8.Click += new System.EventHandler(this.btnGame_8_Click);
+            // 
+            // lblAddToGame
+            // 
+            this.lblAddToGame.AutoSize = true;
+            this.lblAddToGame.Location = new System.Drawing.Point(214, 32);
+            this.lblAddToGame.Name = "lblAddToGame";
+            this.lblAddToGame.Size = new System.Drawing.Size(106, 18);
+            this.lblAddToGame.TabIndex = 27;
+            this.lblAddToGame.Text = "Hinzufügen zu:";
+            // 
+            // FrmHaupt
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(2168, 938);
+            this.Controls.Add(this.lblAddToGame);
+            this.Controls.Add(this.btnGame_8);
+            this.Controls.Add(this.btnGame_7);
+            this.Controls.Add(this.btnGame_6);
+            this.Controls.Add(this.btnGame_5);
+            this.Controls.Add(this.btnGame_4);
+            this.Controls.Add(this.btnGame_3);
+            this.Controls.Add(this.btnGame_2);
+            this.Controls.Add(this.btnGame_1);
+            this.Controls.Add(this.lblGame_8);
+            this.Controls.Add(this.listBoxGame_8);
+            this.Controls.Add(this.lblGame_7);
+            this.Controls.Add(this.listBoxGame_7);
+            this.Controls.Add(this.lblGame_6);
+            this.Controls.Add(this.listBoxGame_6);
+            this.Controls.Add(this.lblGame_5);
+            this.Controls.Add(this.listBoxGame_5);
+            this.Controls.Add(this.lblGame_4);
+            this.Controls.Add(this.listBoxGame_4);
+            this.Controls.Add(this.lblGame_3);
+            this.Controls.Add(this.listBoxGame_3);
+            this.Controls.Add(this.lblGame_2);
+            this.Controls.Add(this.listBoxGame_2);
+            this.Controls.Add(this.lblGame_1);
+            this.Controls.Add(this.listBoxGame_1);
+            this.Controls.Add(this.lblSchüler);
+            this.Controls.Add(this.listBoxSchueler);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "FrmHaupt";
+            this.Text = "Mario Kart Rechner";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBoxSchueler;
+        private System.Windows.Forms.Label lblSchüler;
+        private System.Windows.Forms.Label lblGame_1;
+        private System.Windows.Forms.ListBox listBoxGame_1;
+        private System.Windows.Forms.Label lblGame_2;
+        private System.Windows.Forms.ListBox listBoxGame_2;
+        private System.Windows.Forms.Label lblGame_3;
+        private System.Windows.Forms.ListBox listBoxGame_3;
+        private System.Windows.Forms.Label lblGame_4;
+        private System.Windows.Forms.ListBox listBoxGame_4;
+        private System.Windows.Forms.Label lblGame_5;
+        private System.Windows.Forms.ListBox listBoxGame_5;
+        private System.Windows.Forms.Label lblGame_6;
+        private System.Windows.Forms.ListBox listBoxGame_6;
+        private System.Windows.Forms.Label lblGame_7;
+        private System.Windows.Forms.ListBox listBoxGame_7;
+        private System.Windows.Forms.Label lblGame_8;
+        private System.Windows.Forms.ListBox listBoxGame_8;
+        private System.Windows.Forms.Button btnGame_1;
+        private System.Windows.Forms.Button btnGame_2;
+        private System.Windows.Forms.Button btnGame_3;
+        private System.Windows.Forms.Button btnGame_4;
+        private System.Windows.Forms.Button btnGame_5;
+        private System.Windows.Forms.Button btnGame_6;
+        private System.Windows.Forms.Button btnGame_7;
+        private System.Windows.Forms.Button btnGame_8;
+        private System.Windows.Forms.Label lblAddToGame;
+    }
+}
+
---
+++ MarioKartRechner/FrmHaupt.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MarioKartRechner
+{
+    public partial class FrmHaupt : Form
+    {
+        public FrmHaupt()
+        {
+            InitializeComponent();
+        }
+
+        private void listBoxSchueler_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnGame_1_Click(object sender, EventArgs e)
+        {
+            addSchuelerToGame(listBoxGame_1);
+        }
+
+        private void btnGame_2_Click(object sender, EventArgs e)
+        {
+            addSchuelerToGame(listBoxGame_2);
+
+        }
+
+        private void btnGame_3_Click(object sender, EventArgs e)
+        {
+            addSchuelerToGame(listBoxGame_3);
+        }
+
+        private void btnGame_4_Click(object sender, EventArgs e)
+        {
+            addSchuelerToGame(listBoxGame_4);
+        }
+
+        private void btnGame_5_Click(object sender, EventArgs e)
+        {
+            addSchuelerToGame(listBoxGame_5);
+        }
+
+        private void btnGame_6_Click(object sender, EventArgs e)
+        {
+            addSchuelerToGame(listBoxGame_6);
+        }
+
+        private void btnGame_7_Click(object sender, EventArgs e)
+        {
+            addSchuelerToGame(listBoxGame_7);
+        }
+
+        private void btnGame_8_Click(object sender, EventArgs e)
+        {
+            addSchuelerToGame(listBoxGame_8);
+        }
+
+        private void addSchuelerToGame(ListBox listBox)
+        {
+            if (listBoxSchueler.SelectedIndices.Count == 0)
+                return;
+            listBox.Items.Clear();
+            foreach (int i in listBoxSchueler.SelectedIndices)
+            {
+                listBox.Items.Add(listBoxSchueler.Items[i]);
+
+            }
+
+            //lstClientes.Items.Remove(selectedItems[i]);asd
+        }
+    }
+}
